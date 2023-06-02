0af350ee172e423feeeb6edd3d20af7fd892e199
Jonathan castillo <jonathancastillovelazquez@gmail.com>
2023-06-02 01:07:45 -0600

    test

--- src/models/commonURLs.ts
+++ src/models/commonURLs.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL:string = "https://kanbul-api.vercel.app/api/v1"
-export const BASE_URL:string = "http://localhost:3000"
+export const BASE_URL:string = "http://localhost:3000/api/v1"
