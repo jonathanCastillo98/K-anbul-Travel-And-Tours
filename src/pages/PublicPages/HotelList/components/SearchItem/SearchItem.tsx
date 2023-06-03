2d6e06b762c2d604f6378214abaf5cc138cd1e25
Jonathan castillo <jonathancastillovelazquez@gmail.com>
2023-06-03 01:11:56 -0600

    mobile first

--- src/pages/PublicPages/HotelList/components/SearchItem/SearchItem.tsx
+++ src/pages/PublicPages/HotelList/components/SearchItem/SearchItem.tsx
@@ -28,27 +28,27 @@ const SearchItem = ({ item }: Props) => {
             <div className="siDesc">
                 <h1 className="siTitle">{item.name}</h1>
                 <span className="siDistance">{item.distance} km del centro</span>
-                <span className="siTaxiOp">Free airport taxi</span>
+                <span className="siTaxiOp">Taxi gratis desde el aereopuerto</span>
                 <span className="siSubtitle">
-                    Studio Apartment with Air conditioning
+                    Apartamento tipo estudio con aire acondicionado
                 </span>
                 <span className="siFeatures">
                     {item.description}
                 </span>
-                <span className="siCancelOp">Free cancellation </span>
+                <span className="siCancelOp">Cancelación gratis! </span>
                 <span className="siCancelOpSubtitle">
-                    You can cancel later, so lock in this great price today!
+                    Cancela en cualquier momento, no dejes que este precio se te escape!
                 </span>
             </div>
             <div className="siDetails">
                 {item.rating && <div className="siRating">
-                    <span>Excellent</span>
+                    <span>Excelente</span>
                     <button>{item.rating}</button>
                 </div>}
                 <div className="siDetailTexts">
                     <span className="siPrice">${item.cheapestPrice} MXN</span>
-                    <span className="siTaxOp">Includes taxes and fees</span>
-                    <button className="siCheckButton" onClick={handleOnClick}>See availability</button>
+                    <span className="siTaxOp">Incluye impuestos y honorarios</span>
+                    <button className="siCheckButton" onClick={handleOnClick}>Ver disponibilidad</button>
                 </div>
             </div>
         </div>
